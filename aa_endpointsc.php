c92688db46299ea4a758020d9d3cee1b3d9ea866
Adib AROUI <aroui.adib@gmail.com>
2025-08-12 09:32:21 +0100

    first commit

---
+++ aa_endpointsc.php
@@ -0,0 +1,70 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+class Aa_Endpointsc extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'aa_endpointsc';
+        $this->tab = 'shipping_logistics';
+        $this->author = 'Adib Aroui';
+        $this->version = '1.0.0';
+        $this->need_instance = 0;
+
+        $this->ps_versions_compliancy = [
+            'min' => '1.7',
+            'max' => _PS_VERSION_,
+        ];
+
+        $this->bootstrap = true;
+        parent::__construct();
+
+        $this->displayName = $this->trans('AA SendCloud WebHook Endpoint', [], 'Modules.Endpointsc.Admin');
+        $this->description = $this->trans('This module creates an endpoint URL that will receive notifications from SC webhook', [], 'Modules.Endpointsc.Admin');
+    }
+
+    public function install()
+    {
+        return parent::install()
+            && $this->registerHook('moduleRoutes')
+        ;
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    /**
+     * @param array $params
+     */
+    public function hookModuleRoutes(array $params)
+    {
+
+        return $this->getModuleRoutes('ModuleRoutes', 'customapi');
+    }
+
+    public function getModuleRoutes($ModuleRoutes, $alias)
+    {
+        if ($ModuleRoutes == 'ModuleRoutes') {
+            return array(
+                'module-aa_endpointsc-products' => array(
+                    'controller' => 'notification',
+                    'rule'       => $alias. '/sendcloud' ,
+                    'keywords'   => array(),
+                    'params'     => array(
+                        'fc'     => 'module',
+                        'module' => 'aa_endpointsc',
+                    ),
+                )
+            );
+        }
+    }
+
+}
---
+++ controller/front/notification.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * 2007-2020 PrestaShop and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+use controller\AbstractNotificationController;
+
+require_once __DIR__ . '/../AbstractNotificationController.php';
+
+class Aa_endpointscNotificationModuleFrontController extends AbstractNotificationController
+{
+    protected function processGetRequest()
+    {
+        // do something then output the result
+        $this->ajaxDie(json_encode([
+            'success' => true,
+            'operation' => 'get'
+        ]));
+    }
+
+    protected function processPostRequest()
+    {
+        // do something then output the result
+        $this->ajaxDie(json_encode([
+            'success' => true,
+            'operation' => 'post'
+        ]));
+    }
+
+    protected function processPutRequest()
+    {
+        // do something then output the result
+        $this->ajaxDie(json_encode([
+            'success' => true,
+            'operation' => 'put'
+        ]));
+    }
+
+    protected function processDeleteRequest()
+    {
+        // do something then output the result
+        $this->ajaxDie(json_encode([
+            'success' => true,
+            'operation' => 'delete'
+        ]));
+    }
+}
