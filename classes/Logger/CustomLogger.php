26c3dad84c63bbceddc01ca10011965c879e5af5
Ubuntu <ubuntu@ip-10-0-8-114.eu-west-3.compute.internal>
2025-08-31 03:10:58 +0200

    change log destination

--- classes/Logger/CustomLogger.php
+++ classes/Logger/CustomLogger.php
@@ -3,21 +3,13 @@
 namespace PrestaShop\Module\AaEndpointForSc\Logger;
 
 class CustomLogger {
-    const DEFAULT_LOG_FILE ="prestashop_system.log";
+    const DEFAULT_LOG_FILE ="sendcloud_webhook.log";
     public static function log($message){
-        $fileDir = _PS_ROOT_DIR_ . '/log/';
+        $fileDir = _PS_ROOT_DIR_ . '/var/log/';
         $fileName = self::DEFAULT_LOG_FILE;
-        //var_dump($fileName);
-        //if(is_array($message) || is_object($message)){$message = print_r($message, true);}
-
         ob_start();
-
-// Perform the var_dump()
         var_dump($message);
-
-// Get the buffered content
         $dumpContent = ob_get_clean();
-
 
         return file_put_contents($fileDir . $fileName, $dumpContent, FILE_APPEND);
     }
