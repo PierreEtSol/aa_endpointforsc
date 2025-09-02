6538d03459281796e90beff7e5ca71d38a384ee1
Ubuntu <ubuntu@ip-10-0-8-114.eu-west-3.compute.internal>
2025-09-02 17:04:14 +0200

    take into account all sender addressess ids while fetching SC carriers

--- classes/Services/Carriers/SendCloudCarriers.php
+++ classes/Services/Carriers/SendCloudCarriers.php
@@ -17,21 +17,46 @@ class SendCloudCarriers {
 
         $connection = new Connection($publicKey,  $secretKey);
         $sendCloudClient = new SendCloud($connection);
+
+
+        $senderAddressesIds = $this->getSenderAddressesIds($sendCloudClient);
+        if (!is_array($senderAddressesIds)) {
+            return [];
+        }
         $sendCloudCarriers = [];
+        foreach($senderAddressesIds as $senderAddressId) {
+            $sendCloudCarriers = array_merge($sendCloudCarriers,  $this->getShippingMethods($sendCloudClient, $senderAddressId));
+        }
+
+        $sendCloudCarriers = array_unique($sendCloudCarriers, SORT_REGULAR);
+
+        return $sendCloudCarriers;
+    }
+
+    public function getSenderAddressesIds($sendCloudClient) {
         try {
-
-            $shippingMethods = $sendCloudClient->shippingMethods()->all();
-            foreach ($shippingMethods as $method) {
-                $sendCloudCarriers[] = [
-                    'id_sc_carrier' => $method->id,
-                    'name' => $method->name,
-                ];
-            }
-
-            return $sendCloudCarriers;
+            $senderAddresses = $sendCloudClient->senderAddresses()->all();
+            return array_column($senderAddresses, 'id');
 
         } catch (Exception $e) {
             echo "Error: " . $e->getMessage();
         }
     }
+    public function getShippingMethods($sendCloudClient, $senderAddressId) {
+        $senderShippingMethods = [];
+        try {
+            $shippingMethods = $sendCloudClient->shippingMethods()->all(['sender_address' => $senderAddressId]);
+            foreach ($shippingMethods as $method) {
+                $senderShippingMethods[] = [
+                    'id_sc_carrier' => $method->id,
+                    'name' => $method->name,
+                ];
+            }
+
+            return $senderShippingMethods;
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
 }
